1e0496b8e33b540b717f066455bf949e7a219dcb
Neath <neath@legallywoke.com>
2021-06-08 10:43:17 +1000

    added random messages and getter methods

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // generating random messages
+const randomMessage = {
+    // set available string
+    _myFeelings: ['I am happy', 'I am sad', 'I am angry',
+                 'I am excited', 'I am surprised', 'I am annoyed'],
+    _myThoughts: ['I am loved', 'I am worthwhile', 'I am valued',
+                 'I have been undermined', 'I have been rejected'],
+    _myBehaviors: ['I will eat chocolate', 'I will sleep',
+                'I will go for a run', 'I will dance', 'I will treat myself',
+                'I will cry', 'I will meditate'],
+    // set getter functions
+    get myFeeling(){
+        return this._myFeelings;
+    },
+    get myThoughts(){
+        return this._myThoughts;
+    },
+    get myBehaviors(){
+        return this._myBehaviors;
+    }
+
+}
